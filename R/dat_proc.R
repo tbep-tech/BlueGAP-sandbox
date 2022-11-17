43886d249786b36b019ba37a0567c26f57541d0b
fawda123 <mbafs2012@gmail.com>
2022-11-17 17:30:35 -0500

    added storet data from source

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(here)
+library(readxl)
 
 # raw -----------------------------------------------------------------------------------------
 
@@ -37,3 +38,53 @@ datpro <- datraw %>%
   )
 
 save(datpro, file = here('data/datpro.RData'))
+
+
+# dat raw from FR email 11/17 -----------------------------------------------------------------
+
+# these are from Sennai, apparently just from STORET
+datraw2 <- read_excel('~/Desktop/DEP_AWQ_alldata_11_2021.xlsx')
+
+save(datraw2, file = here('data/datraw2.RData'))
+
+locraw2 <- read_excel('~/Desktop/DEP_AWQ_Station_location.xlsx')
+
+save(locraw2, file = here('data/locraw2.RData'))
+
+# dat processed from FR email 11/17 -----------------------------------------------------------
+
+load(file = here('data/datraw2.RData'))
+load(file = here('data/locraw2.RData'))
+
+locraw2 <- locraw2 %>% 
+  select(
+    MonitoringLocationIdentifier, 
+    ActivityLocation.LatitudeMeasure = LatitudeMeasure, 
+    ActivityLocation.LongitudeMeasure = LongitudeMeasure
+    ) %>% 
+  unique
+
+datpro2 <- datraw2 %>% 
+  filter(ActivityTypeCode %in% c('Field Msr/Obs', 'Sample', 'Sample-Other', 'Sample-Routine')) %>% 
+  filter(grepl('nitra|nitro|nitrite|ammo', CharacteristicName, ignore.case = TRUE)) %>% 
+  # filter(!is.na(ActivityLocation.LatitudeMeasure) & !is.na(ActivityLocation.LongitudeMeasure)) %>% 
+  select(
+    OrganizationIdentifier, 
+    OrganizationFormalName, 
+    ActivityIdentifier,
+    ActivityTypeCode,
+    ActivityMediaSubdivisionName,
+    ActivityStartDate, 
+    ActivityDepthHeightMeasure.MeasureValue = `ActivityDepthHeightMeasure/MeasureValue`,
+    ActivityTopDepthHeightMeasure.MeasureUnitCode = `ActivityTopDepthHeightMeasure/MeasureUnitCode`,
+    CharacteristicName, 
+    ResultMeasureValue, 
+    ResultMeasure.MeasureUnitCode = `ResultMeasure/MeasureUnitCode`, 
+    MeasureQualifierCode, 
+    ResultDetectionConditionText, 
+    MonitoringLocationIdentifier
+  ) %>% 
+  left_join(locraw2, by = 'MonitoringLocationIdentifier') %>% 
+  select(-MonitoringLocationIdentifier)
+
+save(datpro2, file = here('data/datpro2.RData'))
